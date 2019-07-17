9e9ccc2408b438758648d1169270694323be8845
anton <decaseal@gmail.com>
2019-07-17 13:22:00 +0300

    docs and comments

--- pkg/proxy/basic_auth.go
+++ pkg/proxy/basic_auth.go
@@ -3,17 +3,19 @@ package proxy
 import (
 	"encoding/csv"
 	auth "github.com/abbot/go-http-auth"
-	"golang.org/x/net/context"
 	"net/http"
 	"os"
 )
 
+// BasicAuth is an authenticator implementation for basic http authentication.
+// It uses BasicAuth of "github.com/abbot/go-http-auth"
 type BasicAuth struct {
 	Users map[string]string
 
 	basicAuth auth.BasicAuth
 }
 
+// NewBasicAuth load ".htpasswd" file
 func NewBasicAuth(filename string) (*BasicAuth, error) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -39,24 +41,24 @@ func NewBasicAuth(filename string) (*BasicAuth, error) {
 	return basicAuth, nil
 }
 
+// CheckAuth checks the username/password combination from the
+// request. Returns either an empty string (authentication failed) or
+// the name of the authenticated user.
 func (t *BasicAuth) CheckAuth(r *http.Request) string {
 	t.initIfNeed()
 
 	return t.basicAuth.CheckAuth(r)
 }
 
-func (t *BasicAuth) NewContext(ctx context.Context, r *http.Request) context.Context {
-	t.initIfNeed()
-
-	return t.basicAuth.NewContext(ctx, r)
-}
-
+// RequireAuth is an http.HandlerFunc for BasicAuth which initiates
+// the authentication process (or requires reauthentication).
 func (t *BasicAuth) RequireAuth(w http.ResponseWriter, r *http.Request) {
 	t.initIfNeed()
 
 	t.basicAuth.RequireAuth(w, r)
 }
 
+// initIfNeed initiates BasicAuth for use if it's not initiated
 func (t *BasicAuth) initIfNeed() {
 	if t.basicAuth.Secrets == nil {
 		t.basicAuth.Secrets = t.secrets
@@ -64,6 +66,7 @@ func (t *BasicAuth) initIfNeed() {
 	}
 }
 
+// secrets is implementation of SecretProvider of "github.com/abbot/go-http-auth"
 func (t *BasicAuth) secrets(user, realm string) string {
 	password, exists := t.Users[user]
 	if !exists {
